3e9b5418c8998c713f739f5835219cf4790c54c2
nylyhour <lyhuony@gmail.com>
2025-07-24 21:49:14 +0700

    Initial frontend commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import LogInPage from './views/auth/LogInPage.vue';
+
+</script>
+
+<template>
+  <LogInPage />
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/auth/LoginPage.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+
+const formData = reactive({
+  email: "",
+  password: "",
+});
+</script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <div class="w-full max-w-md p-6 rounded-lg shadow-md">
+      <div class="flex justify-center mb-6">
+        <img src="/src/assets/logo.svg" alt="logo" class="h-10" />
+      </div>
+      <h2 class="text-2xl font-bold text-center mb-6 tracking-tight">
+        Hello H5
+      </h2>
+      <form action="" class="space-y-4">
+        <div class="space-y-1">
+          <label for="email" class="block text-sm/6 font-medium text-gray-900"
+            >Email</label
+          >
+          <input
+            type="email"
+            name="email"
+            id="email"
+            v-model="formData.email"
+            class="block w-full px-3 py-1.5 rounded-md text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary"
+            placeholder="Enter your email"
+            required
+            autocomplete="email"
+          />
+        </div>
+
+        <div class="space-y-1">
+          <label
+            for="password"
+            class="block text-sm/6 font-medium text-gray-900"
+            >Password</label
+          >
+          <input
+            type="password"
+            name="password"
+            id="password"
+            v-model="formData.password"
+            class="block w-full px-3 py-1.5 rounded-md text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary"
+            placeholder="Enter your password"
+            required
+            autocomplete="current-password"
+          />
+        </div>
+
+        <button
+          type="submit"
+          class="w-full rounded-md bg-primary px-3 py-1.5 text-base font-semibold text-white hover:bg-hover focus:outline-2 focus:outline-primary transition"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
